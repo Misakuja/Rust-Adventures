4f7f05e914ac214e116b9bce2e6188733b5d1628
misakuja <s30284@pjwstk.edu.pl>
2024-11-02 09:50:50 +0100

    Refactoring and added a true test for quick_sort

---
+++ src/quick_sort.rs
@@ -0,0 +1,38 @@
+fn partitioning(arr: &mut [i32], low: usize, high: usize) -> usize {
+    let pivot = arr[high];
+    let mut i = low as isize - 1;
+
+    for j in low..high {
+        if arr[j] <= pivot {
+            i += 1;
+            arr.swap(i as usize, j);
+        }
+    }
+    arr.swap((i + 1) as usize, high);
+    (i + 1) as usize
+}
+
+fn quicksort(arr: &mut [i32], low: usize, high: usize) {
+    if low < high {
+        let pivot_index = partitioning(arr, low, high);
+        if pivot_index > 0 {
+            quicksort(arr, low, pivot_index - 1);
+        }
+        quicksort(arr, pivot_index + 1, high);
+    }
+}
+
+#[test]
+fn test_quicksort() {
+    let mut quicksort_array: [i32; 10] = [1, 5, 6, 7, 8, 9, 2, 3, 6, 11];
+    let expected: [i32; 10] = [1, 2, 3, 5, 6, 6, 7, 8, 9, 11];
+
+    println!("Before quicksort: {:?}", quicksort_array);
+
+    let len = quicksort_array.len();
+    quicksort(&mut quicksort_array, 0, len - 1);
+
+    println!("After quicksort: {:?}", quicksort_array);
+
+    assert_eq!(quicksort_array, expected)
+}
